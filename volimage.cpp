62e1fc5abe1daf603d01a715d69e4384752aa737
Sindiso Mkhatshwa <mkhsin035@myuct.ac.za>
2020-02-27 21:30:24 +0200

    First version of readImages(std::string) definition

--- volimage.cpp
+++ volimage.cpp
@@ -5,7 +5,12 @@
  */
 
 #include "volimage.h"
+#include <string>
+#include <fstream>
+#include <iterator>
+
 using namespace MKHSIN035;
+using namespace std;
 VolImage::VolImage(){
     this->height = 0;
     this->width = 0;
@@ -16,6 +21,33 @@ VolImage::~VolImage(){
 }
 
 bool VolImage::readImages(std::string baseName){
+    std::string headerfile = "brain_mri_raws/"+baseName+".data";
+    
+    ifstream in(headerfile.c_str());
+    string line;
+    if(!in){
+        cout<<"Could not open "<<headerfile<<endl;
+    }
+    else{
+        cout<<"File opened"<<endl;
+        while(getline(in, line)){
+            istringstream iss(line);
+            vector<string> results(istream_iterator<string>{iss}, istream_iterator<string>());
+            int width, height, numimages;
+            
+            istringstream(results[0])>>width;
+            istringstream(results[1])>>height;
+            istringstream(results[2])>>numimages;
+            
+            for (int i = 0; i < numimages; i++){
+                
+            }
+            
+        }
+    }
+    
+    
+    
     
 }
 
