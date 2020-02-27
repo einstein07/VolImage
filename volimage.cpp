0fff06b97cf6c018d356c1f84e344e72ea09ca28
Sindiso Mkhatshwa <mkhsin035@myuct.ac.za>
2020-02-27 19:18:06 +0200

    Initial commit

---
+++ volimage.cpp
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+#include "volimage.h"
+using namespace MKHSIN035;
+VolImage::VolImage(){
+    this->height = 0;
+    this->width = 0;
+}
+
+VolImage::~VolImage(){
+    
+}
+
+bool VolImage::readImages(std::string baseName){
+    
+}
+
+void VolImage::diffmap(int sliceI, int sliceJ, std::string output_prefix){
+    
+}
+
+void VolImage::extract(int sliceId, std::string output_prefix){
+    
+}
+
+int VolImage::volImageSize(void){
+    
+}
+
+
+
---
+++ volimage.h
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   volimage.h
+ * Author: root07
+ *
+ * Created on 27 February 2020, 18:35
+ */
+
+#ifndef VOLIMAGE_H
+#define VOLIMAGE_H
+#include <vector>
+#include <string>
+namespace MKHSIN035{
+    class VolImage{
+    private: //private members
+        int width, height; //width and height of image stack
+        std::vector<unsigned char**> slices; //data for each slice, in order
+        
+    public: //public members
+        VolImage(); //default constructor
+        ~VolImage(); //destructor
+        
+        //populate the object with images in stack and
+        //set member variables 
+        bool readImages(std::string baseName);
+        
+        //compute difference map and write out
+        void diffmap(int sliceI, int sliceJ, std::string output_prefix);
+        
+        //extract sliceId and write to output
+        void extract(int sliceId, std::string output_prefix);
+        
+        //number of bytes used to store image data bytes
+        //and pointers 
+        int volImageSize(void);
+    };
+}
+
+#endif /* VOLIMAGE_H */
+
